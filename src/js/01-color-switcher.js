efa3af05b6ea3a7fa6626a7be2be43b01ec34775
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-03-12 03:15:33 +0200

    done task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+class colorSwitcher {
+  constructor(start, stop, interval) {
+    this.startBtn = start;
+    this.stopBtn = stop;
+    this.intervalId = interval;
+  }
+  onStartBtnClick() {
+    intervalId = setInterval(() => {
+      bodyRef.style.backgroundColor = `${this.getRandomHexColor()}`;
+    }, 1000);
+    this.startBtn.disabled = true;
+  }
 
+  onStopBtnClick() {
+    clearInterval(intervalId);
+    this.startBtn.disabled = false;
+  }
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+}
+
+const bodyRef = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId = null;
+
+const myColorSwitcher = new colorSwitcher(startBtn, stopBtn, intervalId);
+
+startBtn.addEventListener(
+  'click',
+  myColorSwitcher.onStartBtnClick.bind(myColorSwitcher)
+);
+stopBtn.addEventListener(
+  'click',
+  myColorSwitcher.onStopBtnClick.bind(myColorSwitcher)
+);
