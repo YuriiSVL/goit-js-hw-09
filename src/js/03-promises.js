e892bfc120f9d30f6af274351eaccebb645a7367
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-03-19 22:47:13 +0200

    some change in task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,12 @@
 import Notiflix from 'notiflix';
 
 class PromiseCreator {
-  createPromise(position, delay) {
+  constructor(delay, step, amount) {
+    this.delay = delay;
+    this.step = step;
+    this.amount = amount;
+  }
+  #createPromise(position, delay) {
     const shouldResolve = Math.random() > 0.3;
 
     return new Promise((resolve, reject) => {
@@ -14,6 +19,29 @@ class PromiseCreator {
       }, delay);
     });
   }
+
+  start(e) {
+    e.preventDefault();
+
+    let delay = Number(this.delay.value);
+    const step = Number(this.step.value);
+    const amount = Number(this.amount.value);
+
+    for (let i = 1; i <= amount; i += 1) {
+      this.#createPromise(i, delay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+      delay += step;
+    }
+  }
 }
 
 const refs = {
@@ -23,30 +51,6 @@ const refs = {
   fieldAmount: document.querySelector('[name="amount"]'),
 };
 const { form, fieldDelay, fieldStep, fieldAmount } = refs;
-const promiseCreator = new PromiseCreator();
+const promiseCreator = new PromiseCreator(fieldDelay, fieldStep, fieldAmount);
 
-form.addEventListener('submit', onSubmit);
-
-function onSubmit(e) {
-  e.preventDefault();
-
-  let delay = Number(fieldDelay.value);
-  const step = Number(fieldStep.value);
-  const amount = Number(fieldAmount.value);
-
-  for (let i = 1; i <= amount; i += 1) {
-    promiseCreator
-      .createPromise(i, delay)
-      .then(({ position, delay }) => {
-        Notiflix.Notify.success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`
-        );
-      })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(
-          `❌ Rejected promise ${position} in ${delay}ms`
-        );
-      });
-    delay += step;
-  }
-}
+form.addEventListener('submit', promiseCreator.start.bind(promiseCreator));
